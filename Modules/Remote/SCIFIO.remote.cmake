350304487ef86e22defde269a7ed35a95ec039fb
HongdaZ <zyqlxrhd@sina.cn>
2020-08-17 09:45:52 -0500

    ITK v4.13.3

---
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(SCIFIO
+  "SCIFIO (Bioformats) ImageIO plugin for ITK"
+  GIT_REPOSITORY ${git_protocol}://github.com/scifio/scifio-imageio.git
+  GIT_TAG 05c3a625bd764a49cd2f77c07fcf242f3bf880fe
+  )
